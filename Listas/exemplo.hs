b20f9acc0c3de408f532090d01cd29889e5f5a50
GeovanaS <geovanasilvadasilveira1@gmail.com>
2018-09-27 23:59:47 +0000

    adicionado novas funções

--- Listas/exemplo.hs
+++ Listas/exemplo.hs
@@ -1,5 +1,6 @@
 ---    exemplo.hs
 ---
+
 idade :: Int  -- Um valor inteiro constante
 idade = 17
 
@@ -8,6 +9,12 @@ maiorDeIdade = (idade>=18)
 
 quadrado :: Int -> Int  -- função que eleva num ao quadrado
 quadrado x = x * x       
+
+maiorDeIdade2 :: Int -> Bool
+maiorDeIdade2 i = i>=18
+
+tresiguais :: Int -> Int -> Int -> Bool
+tresiguais x y z = (x == y) && (y == z)
 
 mini :: Int -> Int -> Int  -- função que mostra
 mini a b                   -- o menor entre

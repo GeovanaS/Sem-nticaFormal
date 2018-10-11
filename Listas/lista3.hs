6e5a0d4b2c9633a3539a0e8cc2609823469c91ac
Geovana <geovanasilvadasilveira1@gmail.com>
2018-10-11 15:54:11 -0300

    Update lista3.hs

--- Listas/lista3.hs
+++ Listas/lista3.hs
@@ -5,3 +5,7 @@ somaLista(a:x) = a + somaLista x
 dobraLista :: [Int] -> [Int]
 dobraLista [] = []
 dobraLista(x:xS) = x * x : dobraLista xS
+
+tamanho :: [Int] -> Int
+tamanho [] = 0
+tamanho(x:xC) = 1 + tamanho xC

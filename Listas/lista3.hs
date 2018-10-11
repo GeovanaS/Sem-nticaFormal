fe7f80e582457b1e87551ae0e6b6b7e2912859b4
Geovana <geovanasilvadasilveira1@gmail.com>
2018-10-11 16:04:00 -0300

    Update lista3.hs

--- Listas/lista3.hs
+++ Listas/lista3.hs
@@ -9,3 +9,8 @@ dobraLista(x:xS) = x * x : dobraLista xS
 tamanho :: [Int] -> Int
 tamanho [] = 0
 tamanho(x:xC) = 1 + tamanho xC
+
+produtoLista :: [Int] -> Int
+produtoLista [] = error "lista vazia"
+produtoLista[x] = x
+produtoLista(x:xS) = x * produtoLista xS

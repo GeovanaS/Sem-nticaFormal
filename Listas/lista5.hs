fe90f318ecf059bcc1abe6f6b5458837848c3018
GeovanaS <geovanasilvadasilveira1@gmail.com>
2018-10-18 23:07:48 +0000

    adicionado novos exercicios da lista 5

--- Listas/lista5.hs
+++ Listas/lista5.hs
@@ -23,21 +23,29 @@ area (Circulo r) = pi * r *r
 area (Retangulo b a) = b * a
 
 -- uma arvore binaria de inteiros
-data Arvore = Folha | Nodo Int Arvore Arvore
+data Arvore = Folha Int | Nodo Int Arvore Arvore
+  deriving(Eq,Show)
 -- Uma arvore binaria polimorfica:
 -- data ArvoreP a = Folha | Nodo a (Arvore a) (Arvore a)
 
 -- exemplo
-minhaArvore :: Arvore
-minhaArvore = Nodo 10 (Nodo 14 (Nodo 1 Folha Folha) Folha) Folha
+--minhaArvore :: Arvore
+--minhaArvore = Nodo 10 (Nodo 14 (Nodo 1 Folha Folha) Folha) Folha
 
-somaArvore :: Arvore -> Int 
-somaArvore Folha = 0
+arv1 :: Arvore
+arv1 = Nodo 2 (Nodo 3 (Folha 4) (Folha 2)) (Folha 7)
+
+somaArvore :: Arvore -> Int
+somaArvore (Folha n) = 0
 somaArvore (Nodo n a1 a2) = n + somaArvore a1 + somaArvore a2
 
 -- 1. Defina uma função que multiplique por 2 os inteiros em uma árvore
-multArvore :: Arvore -> Arvore
-multArvore Folha = Folha
-multArvore (Nodo n a1 a2) = Nodo (2*n) (multArvore a1) (multArvore a2)
+multArvore :: Arvore -> Int
+multArvore (Folha n) = n * 2
+multArvore (Nodo n a1 a2) = (n * 2) + multArvore a1  + multArvore a2
 
+-- 2. Defina uma função que conte quantos elementos existem em uma árvore
+contArv :: Arvore -> Int
+contArv (Folha n) = 1
+contArv (Nodo n a1 a2) = 1 + contArv a1 + contArv a2
 

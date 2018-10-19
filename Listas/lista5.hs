3276e4d647b8c80ec8c866c9974334c58a815bc6
GeovanaS <geovanasilvadasilveira1@gmail.com>
2018-10-19 02:21:35 +0000

    lista 5 finalizada

--- Listas/lista5.hs
+++ Listas/lista5.hs
@@ -49,3 +49,43 @@ contArv :: Arvore -> Int
 contArv (Folha n) = 1
 contArv (Nodo n a1 a2) = 1 + contArv a1 + contArv a2
 
+-- 3. Defina uma função que ache o maior elemento de uma árvore
+maiorArv :: Arvore -> Int
+maiorArv (Folha n) = n
+maiorArv (Nodo n a1 a2) 
+  | n > maiorArv a1 && n > maiorArv a2 = n 
+  | maiorArv a1 > maiorArv a2 = maiorArv a1
+  | otherwise = maiorArv a2
+
+-- 4. Defina a função que diz se um inteiro ocorre dentro de uma árvore
+ocorreArv :: Arvore -> Int -> Bool
+ocorreArv (Folha num) n = num == n 
+ocorreArv (Nodo num a1 a2) n = (n == num) || (ocorreArv a1 n)  || (ocorreArv a2 n)
+
+-- 5. Defina uma função que diz quantas vezes um inteiro ocorre dentro de uma árvore
+vezesOcorrencia :: Int -> Arvore -> Int
+vezesOcorrencia n (Folha num) = if(n==num) then 1 else 0
+vezesOcorrencia n (Nodo num a1 a2)
+  | (n == num) = 1 + (vezesOcorrencia n a1) + (vezesOcorrencia n a2)
+  | otherwise = (vezesOcorrencia n a1) + (vezesOcorrencia n a2) 
+
+
+-- 6. Uma árvore refletida é uma árvore com seus ramos esquerdos e direitos trocados. Defina uma função refleteArvore
+refleteArvore :: Arvore -> Arvore
+refleteArvore (Folha n) = (Folha n)
+refleteArvore (Nodo n a1 a2) = Nodo n (refleteArvore a2) (refleteArvore a1)
+
+-- 7. Defina uma função que calcule a altura de uma árvore
+altArv :: Arvore -> Int
+altArv (Folha n) = 0
+altArv (Nodo n a1 a2) = 1 + max(altArv a1) (altArv a2)
+
+-- 8. Defina uma função que transfore uma árvore em uma lista
+listaArv :: Arvore -> [Int]
+listaArv (Folha n) = [n]
+listaArv (Nodo n a1 a2) = [n] ++ (listaArv a1) ++ (listaArv a2)
+
+-- 9.  Defina a função mapTree que aplica uma função a todos os inteiros de todos os nós de uma árvore.
+mapTree :: (Int -> Int) -> Arvore -> Arvore
+mapTree n (Folha num) = (Folha num)
+mapTree n (Nodo num a1 a2) = Nodo (n num) (mapTree n a1) (mapTree n a2)

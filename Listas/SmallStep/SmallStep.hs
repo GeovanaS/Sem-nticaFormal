17695f39ed5957d1eb1e381bb9d58072127a5dfb
GeovanaS <geovanasilvadasilveira1@gmail.com>
2018-11-03 20:00:03 +0000

    Semântica Small-Step

--- Listas/SmallStep/SmallStep.hs
+++ Listas/SmallStep/SmallStep.hs
@@ -58,7 +58,35 @@ smallStepE(Mult e1 e2,s) = let(e,s1) = smallStepE(e1,s)
                                       in(Mult e e2,s)
 
 
+-- Semântica de Expressões Booleanas
 
+-- Not
+smallStepB (Not FALSE,s) = (TRUE,s)
+smallStepB (Not TRUE,s) = (FALSE,s)
+smallStepB (Not b,s) = let(b1,s1) = smallStepB(b,s)
+                                    in(Not b1,s1)
+-- smallStepB (Not (And TRUE FALSE),[("x",0)])
+
+
+-- And 
+
+-- E3
+smallStepB (And FALSE b,s) = (FALSE,s)
+-- E2
+smallStepB (And TRUE b,s) = (b,s)
+-- E1
+smallStepB (And b1 b2,s) = let(b,s1) = smallStepB(b1,s)
+                                       in(And b b2,s1) 
+
+-- Or 
+
+-- OU3
+smallStepB (Or FALSE b,s) = (b,s)
+-- OU2
+smallStepB (Or TRUE b,s) = (TRUE,s)
+-- OU1
+smallStepB (Or b1 b2,s) = let(b,s1) = smallStepB(b1,s)
+                                       in(Or b b2,s1) 
 
 
 
